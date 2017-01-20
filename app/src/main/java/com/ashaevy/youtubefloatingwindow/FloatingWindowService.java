41cc6ab266852fe5006748df96edbc919a9dd044
Yuriy Ashaev <ashaevy@gmail.com>
2017-01-20 05:22:48 +0200

    Added sample with Fragment.

--- app/src/main/java/com/ashaevy/youtubefloatingwindow/FloatingWindowService.java
+++ app/src/main/java/com/ashaevy/youtubefloatingwindow/FloatingWindowService.java
@@ -1,10 +1,15 @@
 package com.ashaevy.youtubefloatingwindow;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
+import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -16,19 +21,66 @@ import android.widget.LinearLayout;
 
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 public class FloatingWindowService extends Service {
 
-    WindowManager wm;
-    LinearLayout ll;
+    public static final String YT_ACTIVITY_CREATED = "YT_ACTIVITY_CREATED";
+    public static final String YT_ACTIVITY_STARTED = "YT_ACTIVITY_STARTED";
+    public static final String YT_ACTIVITY_STOPPED = "YT_ACTIVITY_STOPPED";
+    public static final String YT_ACTIVITY_DESTROYED = "YT_ACTIVITY_DESTROYED";
 
-    FakeYouTubeBaseActivity fakeYouTubeBaseActivity;
-    YouTubePlayerView youTubePlayerView;
+    private WindowManager wm;
+    private LinearLayout ll;
+
+    private FakeYouTubeBaseActivity fakeYouTubeBaseActivity;
+    private YouTubePlayerView youTubePlayerView;
+
+    private View youTubeFragmentView;
+    private YouTubePlayerFragment youTubePlayerFragment;
+
+    private int overlayType = -1;
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+        if (YT_ACTIVITY_STOPPED.equals(intent.getAction())) {
+            addPlayer(intent);
+        } else if (YT_ACTIVITY_CREATED.equals(intent.getAction()) ||
+                YT_ACTIVITY_STARTED.equals(intent.getAction()) ||
+                YT_ACTIVITY_DESTROYED.equals(intent.getAction())) {
+            removePlayer();
+        }
+        }
+    };
+
+    private void removePlayer() {
+        if (overlayType == MainActivity.OT_VIEW) {
+            removeYTView();
+        } else {
+            removeYTFragment();
+        }
+    }
+
+    private void addPlayer(Intent intent) {
+        if (overlayType == MainActivity.OT_VIEW) {
+            addYTView();
+        } else {
+            addYTFragment(intent);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        overlayType = intent.getIntExtra(MainActivity.OVERLAY_TYPE, -1);
+
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
@@ -37,7 +89,46 @@ public class FloatingWindowService extends Service {
 
         wm = (WindowManager) getSystemService(WINDOW_SERVICE);
 
-        ll = new LinearLayout(this);
+        final WindowManager.LayoutParams parameters = new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_TOAST,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        ll = new LinearLayout(this) {
+            WindowManager.LayoutParams updatedParameters = parameters;
+            double x;
+            double y;
+            double pressedX;
+            double pressedY;
+
+            @Override
+            public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+                switch (ev.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        x = updatedParameters.x;
+                        y = updatedParameters.y;
+
+                        pressedX = ev.getRawX();
+                        pressedY = ev.getRawY();
+
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        updatedParameters.x = (int) (x + (ev.getRawX() - pressedX));
+                        updatedParameters.y = (int) (y + (ev.getRawY() - pressedY));
+
+                        wm.updateViewLayout(ll, updatedParameters);
+
+                    default:
+                        break;
+                }
+
+                return false;
+
+            }
+        };
         ll.setBackgroundColor(Color.RED);
         LinearLayout.LayoutParams layoutParameteres = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT, 1000);
@@ -45,17 +136,12 @@ public class FloatingWindowService extends Service {
         ll.setLayoutParams(layoutParameteres);
         ll.setOrientation(LinearLayout.VERTICAL);
 
-        final WindowManager.LayoutParams parameters = new WindowManager.LayoutParams(
-                800, 1200, WindowManager.LayoutParams.TYPE_TOAST,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
-
         parameters.gravity = Gravity.CENTER | Gravity.CENTER;
         parameters.x = 0;
         parameters.y = 0;
 
-      Button     stop = new Button(this);
-    stop.setText("Stop");
+        Button     stop = new Button(this);
+        stop.setText("Stop");
         ViewGroup.LayoutParams btnParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
         stop.setLayoutParams(btnParameters);
 
@@ -67,37 +153,9 @@ public class FloatingWindowService extends Service {
         addPlayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    if (fakeYouTubeBaseActivity != null) {
-                        return;
-                    }
-
-                    fakeYouTubeBaseActivity = new FakeYouTubeBaseActivity();
-                    fakeYouTubeBaseActivity.init(FloatingWindowService.this, wm);
-
-                    youTubePlayerView = new YouTubePlayerView(fakeYouTubeBaseActivity);
-                    youTubePlayerView.initialize(getString(R.string.you_tube_access_id), new YouTubePlayer.OnInitializedListener() {
-                        @Override
-                        public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-                            youTubePlayer.cueVideo("fhWaJi1Hsfo");
-                        }
-
-                        @Override
-                        public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-                            Log.e(FloatingWindowService.class.getSimpleName(), "Can't initialize YouTubePlayerView.");
-                        }
-                    });
-                    youTubePlayerView.setLayoutParams(new LinearLayout.LayoutParams(800, 500));
-
-                    ll.addView(youTubePlayerView);
-
-                    fakeYouTubeBaseActivity.start();
-
-                } catch (Exception e) {
-                    Log.e(FloatingWindowService.class.getSimpleName(), "Can't setup YouTubeActivity: ", e);
-                    fakeYouTubeBaseActivity = null;
-                    youTubePlayerView = null;
-                }
+                Intent intent = new Intent();
+                intent.putExtra(MainActivity.VIDEO_ID, MainActivity.DEFAULT_VIDEO);
+                addPlayer(intent);
             }
         });
 
@@ -109,18 +167,7 @@ public class FloatingWindowService extends Service {
         removePlayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (fakeYouTubeBaseActivity != null) {
-                    ll.removeView(youTubePlayerView);
-                    youTubePlayerView = null;
-                    try {
-                        fakeYouTubeBaseActivity.stop();
-                        fakeYouTubeBaseActivity.destroy();
-                    } catch (Exception e) {
-                        Log.e(FloatingWindowService.class.getSimpleName(), "Can't stop YouTubeActivity: ", e);
-                    } finally {
-                        fakeYouTubeBaseActivity = null;
-                    }
-                }
+                removePlayer();
             }
         });
 
@@ -131,54 +178,150 @@ public class FloatingWindowService extends Service {
         IBinder applicationWindowToken = ll.getApplicationWindowToken();
         Log.i(FloatingWindowService.class.getSimpleName(), "Application token of ll: " + applicationWindowToken);
 
-        ll.setOnTouchListener(new View.OnTouchListener() {
-            WindowManager.LayoutParams updatedParameters = parameters;
-            double x;
-            double y;
-            double pressedX;
-            double pressedY;
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-
-                        x = updatedParameters.x;
-                        y = updatedParameters.y;
-
-                        pressedX = event.getRawX();
-                        pressedY = event.getRawY();
-
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        updatedParameters.x = (int) (x + (event.getRawX() - pressedX));
-                        updatedParameters.y = (int) (y + (event.getRawY() - pressedY));
-
-                        wm.updateViewLayout(ll, updatedParameters);
-
-                    default:
-                        break;
-                }
-
-                return false;
-            }
-        });
-
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 wm.removeView(ll);
                 stopSelf();
-                System.exit(0);
             }
         });
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(YT_ACTIVITY_CREATED);
+        intentFilter.addAction(YT_ACTIVITY_STARTED);
+        intentFilter.addAction(YT_ACTIVITY_STOPPED);
+        intentFilter.addAction(YT_ACTIVITY_DESTROYED);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    private void removeYTView() {
+        if (fakeYouTubeBaseActivity != null) {
+            ll.removeView(youTubePlayerView);
+            youTubePlayerView = null;
+            try {
+                fakeYouTubeBaseActivity.stop();
+                fakeYouTubeBaseActivity.destroy();
+            } catch (Exception e) {
+                Log.e(FloatingWindowService.class.getSimpleName(), "Can't stop YouTubeActivity: ", e);
+            } finally {
+                fakeYouTubeBaseActivity = null;
+            }
+        }
+    }
+
+    private void addYTView() {
+        try {
+            if (fakeYouTubeBaseActivity != null) {
+                return;
+            }
+
+            fakeYouTubeBaseActivity = new FakeYouTubeBaseActivity();
+            fakeYouTubeBaseActivity.init(FloatingWindowService.this, wm);
+
+            youTubePlayerView = new YouTubePlayerView(fakeYouTubeBaseActivity);
+            youTubePlayerView.initialize(getString(R.string.you_tube_access_id), new YouTubePlayer.OnInitializedListener() {
+                @Override
+                public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                    youTubePlayer.cueVideo("fhWaJi1Hsfo");
+                }
+
+                @Override
+                public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                    Log.e(FloatingWindowService.class.getSimpleName(), "Can't initialize YouTubePlayerView.");
+                }
+            });
+            youTubePlayerView.setLayoutParams(new LinearLayout.LayoutParams(800, 500));
+
+            ll.addView(youTubePlayerView);
+
+            fakeYouTubeBaseActivity.start();
+
+        } catch (Exception e) {
+            Log.e(FloatingWindowService.class.getSimpleName(), "Can't setup YouTubeActivity: ", e);
+            fakeYouTubeBaseActivity = null;
+            youTubePlayerView = null;
+        }
+    }
+
+    private void removeYTFragment() {
+        if (youTubePlayerFragment != null) {
+            ll.removeView(youTubeFragmentView);
+            youTubeFragmentView = null;
+            youTubePlayerFragment = null;
+        }
+    }
+
+    private void addYTFragment(final Intent intent) {
+        if (youTubePlayerFragment != null) {
+            return;
+        }
+
+        youTubePlayerFragment = MainActivity.getYouTubePlayerFragment();
+        if (youTubePlayerFragment != null) {
+            youTubePlayerFragment.onCreate(null);
+            youTubeFragmentView = youTubePlayerFragment.onCreateView(null, null, null);
+            ll.addView(youTubeFragmentView);
+
+            youTubePlayerFragment.initialize(getString(R.string.you_tube_access_id), new YouTubePlayer.OnInitializedListener() {
+                @Override
+                public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                    final YouTubePlayer youTubePlayer, boolean b) {
+                    String videoId = intent.getStringExtra(MainActivity.VIDEO_ID);
+                    if (videoId != null) {
+                        youTubePlayer.cueVideo(videoId);
+                        youTubePlayer.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                            @Override
+                            public void onLoading() {
+
+                            }
+
+                            @Override
+                            public void onLoaded(String s) {
+                                final int time = intent.getIntExtra(MainActivity.TIMESTAMP, -1);
+                                if (time != -1) {
+                                    youTubePlayer.seekToMillis(time);
+                                    youTubePlayer.play();
+                                }
+                            }
+
+                            @Override
+                            public void onAdStarted() {
+
+                            }
+
+                            @Override
+                            public void onVideoStarted() {
+
+                            }
+
+                            @Override
+                            public void onVideoEnded() {
+
+                            }
+
+                            @Override
+                            public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+                            }
+                        });
+                    }
+                }
+
+                @Override
+                public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                    Log.e(FloatingWindowService.class.getSimpleName(), "Can't initialize YouTubePlayerView.");
+                }
+            });
+            youTubePlayerFragment.onStart();
+            youTubePlayerFragment.onResume();
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
         stopSelf();
     }
 
--- app/src/main/java/com/ashaevy/youtubefloatingwindow/MainActivity.java
+++ app/src/main/java/com/ashaevy/youtubefloatingwindow/MainActivity.java
@@ -2,37 +2,179 @@ package com.ashaevy.youtubefloatingwindow;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 
 public class MainActivity extends YouTubeBaseActivity {
 
+    public static final String YOUTUBE_FRAGMENT_TAG = YouTubePlayerFragment.class.getSimpleName();
+    public static final String VIDEO_ID = "video_id";
+    public static final String TIMESTAMP = "timestamp";
+
+    public static final String DEFAULT_VIDEO = "fhWaJi1Hsfo";
+
+    public static final String KEY_VIDEO_TIMESTAMP = "KEY_VIDEO_TIMESTAMP";
+    public static final String OVERLAY_TYPE = "overlay_type";
+    public static final int OT_VIEW = 0;
+    public static final int OT_FRAGMENT = 1;
+
     Button b;
+
+    private static YouTubePlayerFragment floatingYouTubePlayerFragment;
+
+    private YouTubePlayer youTubePlayer;
+
+    private String currentVideoId;
+    private int currentVideoTimestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(FloatingWindowService.
+                YT_ACTIVITY_CREATED));
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        b = (Button) findViewById(R.id.btn);
-
-        b.setOnClickListener(new View.OnClickListener() {
-
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-            startService(new Intent(MainActivity.this,FloatingWindowService.class));
-            onBackPressed();
+                Intent serviceIntent = new Intent(MainActivity.this, FloatingWindowService.class);
+                if (arg0.getId() == R.id.btn_view) {
+                    serviceIntent.putExtra(OVERLAY_TYPE, OT_VIEW);
+                } else if (arg0.getId() == R.id.btn_fragment) {
+                    serviceIntent.putExtra(OVERLAY_TYPE, OT_FRAGMENT);
+                }
+                startService(serviceIntent);
+            }
+        };
+        findViewById(R.id.btn_view).setOnClickListener(onClickListener);
+        findViewById(R.id.btn_fragment).setOnClickListener(onClickListener);
+
+        currentVideoId = DEFAULT_VIDEO;
+    }
+
+    @Override
+    protected void onStart() {
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(FloatingWindowService.
+                YT_ACTIVITY_STARTED));
+        super.onStart();
+
+        YouTubePlayerFragment retainedFragment = ((YouTubePlayerFragment) getFragmentManager().
+                findFragmentByTag(YOUTUBE_FRAGMENT_TAG));
+
+        if (retainedFragment != null) {
+            getFragmentManager().beginTransaction().remove(retainedFragment).commit();
+        }
+
+        floatingYouTubePlayerFragment = new YouTubePlayerFragment();
+        floatingYouTubePlayerFragment.setRetainInstance(true);
+
+        getFragmentManager().beginTransaction().add(R.id.youtube_fragment_layout,
+                floatingYouTubePlayerFragment, YOUTUBE_FRAGMENT_TAG).commit();
+
+        floatingYouTubePlayerFragment.initialize(getString(R.string.you_tube_access_id), new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                final YouTubePlayer youTubePlayer, boolean b) {
+                MainActivity.this.youTubePlayer = youTubePlayer;
+                youTubePlayer.cueVideo(currentVideoId);
+                youTubePlayer.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                    @Override
+                    public void onLoading() {
+
+                    }
+
+                    @Override
+                    public void onLoaded(String s) {
+                        if (currentVideoTimestamp != 0) {
+                            youTubePlayer.seekToMillis(currentVideoTimestamp);
+                            youTubePlayer.play();
+                        }
+                    }
+
+                    @Override
+                    public void onAdStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoEnded() {
+
+                    }
+
+                    @Override
+                    public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.e(FloatingWindowService.class.getSimpleName(), "Can't initialize YouTubePlayerView.");
             }
         });
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            int timeStamp = savedInstanceState.getInt(KEY_VIDEO_TIMESTAMP);
+            if (timeStamp != 0) {
+                currentVideoTimestamp = timeStamp;
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+
+        if (youTubePlayer != null) {
+            currentVideoTimestamp = youTubePlayer.getCurrentTimeMillis();
+            bundle.putInt(KEY_VIDEO_TIMESTAMP, currentVideoTimestamp);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        Intent intent = new Intent(FloatingWindowService.
+                YT_ACTIVITY_STOPPED);
+        if (currentVideoId != null) {
+            intent.putExtra(VIDEO_ID, currentVideoId);
+            if (youTubePlayer != null) {
+                intent.putExtra(TIMESTAMP, youTubePlayer.getCurrentTimeMillis());
+            }
+        }
+        youTubePlayer = null;
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(FloatingWindowService.
+                YT_ACTIVITY_DESTROYED));
+        super.onDestroy();
+        floatingYouTubePlayerFragment = null;
+    }
+
+    public static YouTubePlayerFragment getYouTubePlayerFragment() {
+        return floatingYouTubePlayerFragment;
     }
 
 }
